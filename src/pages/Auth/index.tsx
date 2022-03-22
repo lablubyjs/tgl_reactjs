41b842e3cc668d66a65b45749f70f494a1cf67b5
Crislâine Santos <criis.santos@outlook.com>
2022-03-22 18:16:13 -0300

    feat(pages): add Auth page

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,4 +1,37 @@
+import Container from '@components/Container';
+import Content from '@components/Content';
+import Footer from '@components/Footer';
+import Form from '@components/Form/indext';
+import Slogan from '@components/Slogan';
+import { Link, useNavigate } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
 
 export default function Auth() {
-   return <div>Auth Page</div>
+	const navigate = useNavigate();
+
+	const goToHandler = () => {
+		navigate('/registration');
+	};
+
+	return (
+		<Container>
+			<Content>
+				<Slogan />
+				<Form
+					title='Authentication'
+					goTo='Sing Up'
+					onGoTo={goToHandler}
+					arrow='right'>
+					<input placeholder='Email' />
+					<input placeholder='Password' />
+					<Link to='/reset-password'>I forget my password</Link>
+					<button>
+						Log In
+						<BsArrowRight />
+					</button>
+				</Form>
+			</Content>
+			<Footer />
+		</Container>
+	);
 }
