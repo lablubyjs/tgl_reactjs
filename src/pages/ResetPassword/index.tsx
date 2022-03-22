dae78b27d1d3d45679e3dd218bb5e6c6b4407771
Crislâine Santos <criis.santos@outlook.com>
2022-03-22 18:17:09 -0300

    feat(pages): add ResetPassword

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import Container from '@components/Container';
+import Content from '@components/Content';
+import Footer from '@components/Footer';
+import Form from '@components/Form/indext';
+import Slogan from '@components/Slogan';
+import { BsArrowRight } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 function ResetPassword() {
-	return <div>Reset Password Page</div>;
+	const navigate = useNavigate();
+
+	const goToHandler = () => {
+		navigate('/auth');
+	};
+
+	return (
+		<Container>
+			<Content>
+				<Slogan />
+				<Form
+					title='Registration'
+					goTo='Back'
+					onGoTo={goToHandler}
+					arrow='left'>
+					<input placeholder='Name' />
+					<input placeholder='Email' />
+					<input placeholder='Password' />
+					<button>
+						Register
+						<BsArrowRight />
+					</button>
+				</Form>
+			</Content>
+			<Footer />
+		</Container>
+	);
 }
 
 export default ResetPassword;
