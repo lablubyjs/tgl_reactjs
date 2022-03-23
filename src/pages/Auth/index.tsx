71d24db10095579188da43dc8126c5dbbe5e7e9c
Crislâine Santos <criis.santos@outlook.com>
2022-03-23 18:02:47 -0300

    feat(auth): adding field validation and login function

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,35 +1,101 @@
 import Container from '@components/Container';
 import Content from '@components/Content';
 import Footer from '@components/Footer';
-import Form from '@components/Form/indext';
+import Form from '@components/Form';
 import Slogan from '@components/Slogan';
+import { ContainerForm, Title } from '@components/Form/style';
+import { yupResolver } from '@hookform/resolvers/yup';
+import InputContainer from '@components/InputContainer';
+import Button from '@components/Button';
+
+import { authServices } from '../../shared/services';
+
+import { toast } from 'react-toastify';
+
 import { Link, useNavigate } from 'react-router-dom';
-import { BsArrowRight } from 'react-icons/bs';
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+
+import { FormValues } from '../../types/index';
 
 export default function Auth() {
 	const navigate = useNavigate();
+	const { login } = authServices();
+
+	const schema = yup.object().shape({
+		email: yup.string().email('Invalid email').required('Informe um email'),
+		password: yup.string().required('Informe a senha'),
+	});
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>({
+		resolver: yupResolver(schema),
+	});
 
 	const goToHandler = () => {
 		navigate('/registration');
+	};
+
+	const onLoginHandler = async (data: FormValues) => {
+		console.log(data);
+
+		try {
+			const response = await toast.promise(
+				login({
+					email: data.email,
+					password: data.password!,
+				}),
+				{
+					pending: 'Carregando',
+					success: 'Autenticado com sucesso',
+					error: 'Falha na autenticação',
+				}
+			);
+
+			navigate('/bets');
+		} catch (error: any) {
+			if (error.status === 401) {
+				toast.error(error.data.message);
+			}
+		}
 	};
 
 	return (
 		<Container>
 			<Content>
 				<Slogan />
-				<Form
-					title='Authentication'
-					goTo='Sing Up'
-					onGoTo={goToHandler}
-					arrow='right'>
-					<input placeholder='Email' />
-					<input placeholder='Password' />
-					<Link to='/reset-password'>I forget my password</Link>
-					<button>
-						Log In
-						<BsArrowRight />
-					</button>
-				</Form>
+				<ContainerForm>
+					<Title>Autentication</Title>
+					<Form onSubmit={handleSubmit(onLoginHandler)}>
+						<InputContainer error={errors.email}>
+							<input placeholder='Email' {...register('email')} />
+						</InputContainer>
+						<InputContainer error={errors.password}>
+							<input
+								placeholder='Password'
+								type='password'
+								{...register('password')}
+							/>
+						</InputContainer>
+						<Link to='/reset-password'>I forget my password</Link>
+						<Button
+							goTo='Log In'
+							arrow='rigth'
+							fontSize={32}
+							color='var(--light-green)'
+						/>
+					</Form>
+					<Button
+						goTo='Sing Up'
+						arrow='rigth'
+						onGoTo={goToHandler}
+						fontSize={35}
+						color='var(--grey)'
+					/>
+				</ContainerForm>
 			</Content>
 			<Footer />
 		</Container>
