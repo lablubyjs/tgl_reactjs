098ee59ba516428fa1d0a4ed0faaf85700b46148
Crislâine Santos <criis.santos@outlook.com>
2022-03-23 18:11:47 -0300

    feat(app): react toastify to add feedbacks to user added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import ContainerRoutes from '@routes/index';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css'
 
 function App() {
-	return <ContainerRoutes />;
+	return (
+		<main>
+			<ContainerRoutes />
+			<ToastContainer />
+		</main>
+	);
 }
 
 export default App;
