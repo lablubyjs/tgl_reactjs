5b02ce71b00291c40b1e68221c1c983d6d397ea1
Crislâine Santos <criis.santos@outlook.com>
2022-03-24 09:56:12 -0300

    feat(services): user services updated

--- src/shared/services/user/index.ts
+++ src/shared/services/user/index.ts
@@ -1,13 +1,26 @@
-import { IBodyUser, ICreateUserResponse } from '../../interfaces';
+import {
+	IBodyUser,
+	ICreateUserResponse,
+	IMyAccountResponse,
+	IUpdateUserResponse,
+} from '@shared/interfaces/UserInterfaces';
 import instance from '../axios.config';
 import { IUser } from './interfaces';
 
-const createUserServices = (): IUser => {
+const userServices = (): IUser => {
 	async function createUser(body: IBodyUser): Promise<ICreateUserResponse> {
 		return instance.post('/user/create', body);
 	}
 
-	return { createUser };
+	async function updateMyUser(body: IBodyUser): Promise<IUpdateUserResponse> {
+		return instance.put('/user/update', body);
+	}
+
+	async function myAccount(): Promise<IMyAccountResponse> {
+		return instance.get('/user/my-account');
+	}
+
+	return { createUser, updateMyUser, myAccount };
 };
 
-export default createUserServices;
+export default userServices;
--- src/shared/services/user/interfaces.ts
+++ src/shared/services/user/interfaces.ts
@@ -1,6 +1,9 @@
 import { IBodyUser, ICreateUserResponse, ILoginResponse } from '@shared/interfaces';
-import { IResetResponse } from '@shared/interfaces/AuthInterfaces';
+import { IMyAccountResponse, IUpdateUserResponse } from '@shared/interfaces/UserInterfaces';
+import { AxiosRequestHeaders } from 'axios';
 
 export interface IUser {
 	createUser: ({ email, password, name }: IBodyUser) => Promise<ICreateUserResponse>;
+	updateMyUser: ({ email, name }: IBodyUser) => Promise<IUpdateUserResponse>;
+	myAccount: () => Promise<IMyAccountResponse>;
 }
