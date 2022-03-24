df5ec2dea344180ddbb830dd91af4a80a7f70206
Crislâine Santos <criis.santos@outlook.com>
2022-03-24 18:25:51 -0300

    feat(routes): protect routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,16 +6,21 @@ import Bets from '@pages/Bets';
 import Game from '@pages/Game';
 import Registration from '@pages/Registration';
 import ResetPassword from '@pages/ResetPassword';
+import ProtectRouter from 'src/helper';
+import { useAppSelector } from 'src/hooks';
 
 export default function ContainerRoutes() {
+
+	const token = localStorage.getItem('token')
+	
 	return (
 		<Routes>
-			<Route path='/' element={<Navigate to='/auth' />} />
+			<Route path='/*' element={!token ? <Navigate to='/auth' /> : <Navigate to='/home' />} />
 			<Route path='/auth' element={<Auth />} />
 			<Route path='/registration' element={<Registration />} />
 			<Route path='/reset-password' element={<ResetPassword />} />
-			<Route path='/game' element={<Game />} />
-			<Route path='/bets' element={<Bets />} />
+			<Route path='/games' element={<ProtectRouter component={<Game />} />} />
+			<Route path='/home' element={<ProtectRouter component={<Bets />} />} />
 		</Routes>
 	);
 }
