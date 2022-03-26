a0bc669184cda30ecf951ffdf679c2423519a49e
Crislâine Santos <criis.santos@outlook.com>
2022-03-25 23:18:31 -0300

    style(content): replace margin-top to container content

--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -4,7 +4,7 @@ export const ContainerContent = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr;
 	padding: 3rem;
-	margin-top: 50px;
+	margin-top: 45px;
 
 	@media (max-width: 820px) {
 		grid-template-columns: 1fr;
