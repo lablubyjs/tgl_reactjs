3764bd7488c68e1aa6930daf7a4b1e4476cdb2a4
Crislâine Santos <criis.santos@outlook.com>
2022-03-25 23:24:33 -0300

    feat(store): async function to get list games added

--- src/store/games-slice.ts
+++ src/store/games-slice.ts
@@ -1,20 +1,38 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { IListGamesReponse } from '@shared/interfaces';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { gamesServices } from '@shared/services';
 
-const initialGamesState: IListGamesReponse['types'] = [];
+const { listGames } = gamesServices();
+
+const initalGamesState: any = {
+	list: [],
+};
 
 const gamesSlice = createSlice({
 	name: 'games',
 
-	initialState: initialGamesState,
+	initialState: initalGamesState,
 
 	reducers: {
-		addGames(state, action: PayloadAction<IListGamesReponse['types']>) {
-			state = action.payload;
+		addGames(state, action: PayloadAction<any>) {
+			console.log('adding');
+			state.list = action.payload;
 		},
+	},
+
+	extraReducers: (builder) => {
+		builder.addCase(asyncAddGames.fulfilled, (state, action) => {
+			state.list = action.payload;
+		});
 	},
 });
 
 export const gamesActions = gamesSlice.actions;
-
 export default gamesSlice.reducer;
+
+export const asyncAddGames = createAsyncThunk(
+	'games/fetchAddGames',
+	async () => {
+		const response = await listGames();
+		return response.types;
+	}
+);
