78f7ec6b85796f36812d487c2a0312ac674716ec
Crislâine Santos <criis.santos@outlook.com>
2022-03-25 23:39:57 -0300

    feat(pages): header component and games component added to bets page

--- src/pages/Bets/index.tsx
+++ src/pages/Bets/index.tsx
@@ -1,7 +1,17 @@
+import BetsContainer from '@components/BetsContainer';
+import Container from '@components/Container';
+import Footer from '@components/Footer';
+import Header from '@components/Header';
 import React from 'react';
 
 function Bets(): JSX.Element {
-	return <div>Bets Page</div>;
+	return (
+		<Container>
+			<Header showHomeLink={false} />
+			<BetsContainer />
+			<Footer />
+		</Container>
+	);
 }
 
 export default Bets;
