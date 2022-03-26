e6f93f8ca0a8754e80e53d95d29a5f0c8dd0b18e
Crislâine Santos <criis.santos@outlook.com>
2022-03-25 23:15:38 -0300

    feat(components): betsContainer component added

---
+++ src/components/BetsContainer/index.tsx
@@ -0,0 +1,13 @@
+import ButtonGame from '@components/Games/ButtonGame';
+import ButtonsGames from '@components/Games/ButtonsGames';
+import React from 'react';
+
+const BetsContainer = (): JSX.Element => {
+	return (
+		<div>
+			<ButtonsGames />
+		</div>
+	);
+};
+
+export default BetsContainer;
