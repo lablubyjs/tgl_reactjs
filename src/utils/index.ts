67ba4d9a67428f17a7a9a3ea748eac2413101a76
Crislâine Santos <criis.santos@outlook.com>
2022-03-27 16:57:43 -0300

    feat: utils functions added

---
+++ src/utils/index.ts
@@ -0,0 +1,50 @@
+const valueFormat = (type: string, value: string) => {
+	let valueFormated;
+
+	if (type === 'DATE') {
+		const date = new Date(value);
+
+		const day = date.getDate();
+		const month =
+			date.getMonth() < 9
+				? `0${date.getMonth() + 1}`
+				: `${date.getMonth() + 1}`;
+		const year = date.getFullYear();
+
+		valueFormated = `${day}/${month}/${year}`;
+	}
+
+	if (type === 'PRICE') {
+		const number = Number(value);
+
+		const price = number.toLocaleString('pt-br', {
+			style: 'currency',
+			currency: 'BRL',
+		});
+
+		valueFormated = price;
+	}
+
+	if (type === 'NUMBERS') {
+		const numbers = value.split(',');
+
+		const arrayNumbers = numbers.map((item) => {
+			const number = parseFloat(item);
+			return number < 10 ? `0${number}` : number;
+		});
+
+		valueFormated = arrayNumbers.join(', ');
+	}
+
+	return valueFormated;
+};
+
+export const getGameColor = (games: any, id: number) => {
+	const gameSelected = games.find((game: any) => {
+		return game.id === id;
+	});
+
+	return gameSelected.color;
+};
+
+export default valueFormat;
