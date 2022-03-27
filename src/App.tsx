97122b2ec96f7258c25af83bf1094e81849785de
Crislâine Santos <criis.santos@outlook.com>
2022-03-27 17:06:33 -0300

    feat(app.tsx): useEffect and dispatch function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ContainerRoutes from '@routes/index';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-toastify/dist/ReactToastify.min.css';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { asyncAddGames } from '@store/games-slice';
+import { asyncAddBets } from '@store/bets-slice';
 
 function App() {
+	const dispatch = useAppDispatch();
+	const url = useAppSelector(state => state.bets.querys.join(''))
+
+	useEffect(() => {
+		dispatch(asyncAddGames());
+		dispatch(asyncAddBets(url));
+	}, []);
+
 	return (
 		<main>
 			<ContainerRoutes />
