123191524640d4e7119671b54b7e42604a3c8c71
Crislâine Santos <criis.santos@outlook.com>
2022-03-28 11:14:54 -0300

    style(footer): min height added

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -4,6 +4,7 @@ export const FooterContainer = styled.footer`
    text-align: center;
    border-top: 2px solid #EBEBEB;
    padding-top: 10px;
+   min-height: 4rem;
 `
 
 export const Text = styled.p`
