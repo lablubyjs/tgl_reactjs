2442e4817306af3ed63ccfdec2d50a413ce339ae
Crislâine Santos <criis.santos@outlook.com>
2022-03-29 11:59:08 -0300

    feat(components): cart items added

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,16 +1,116 @@
 import Button from '@components/Button';
 import EmptyMessage from '@components/EmptyMessage';
-import React from 'react';
-import { useAppSelector } from 'src/hooks';
-import { ButtonSave, ContainerCart } from './style';
+import Modal from '@components/Modal';
+import React, { useState } from 'react';
+import { useAppDispatch, useAppSelector } from 'src/hooks';
+import valueFormat, {
+	getGameColor,
+	getGameName,
+	getGamePrice,
+	getGamePriceNumber,
+} from 'src/utils';
+import {
+	ButtonSave,
+	ButtonTrash,
+	CartTotal,
+	ContainerCart,
+	GameInfo,
+	GamesContainer,
+	Content,
+	ButtonModal,
+	ContentModal,
+} from './style';
+import { betsServices } from '@shared/services';
+import { toast } from 'react-toastify';
+import { asyncAddBets } from '@store/bets-slice';
+import { useNavigate } from 'react-router-dom';
+import { IoTrashOutline } from 'react-icons/io5';
+import { IoAlertCircleOutline } from 'react-icons/io5';
+import { removeToCart } from '@store/cart-slice';
 
 const Cart = () => {
 	const gamesInCart = useAppSelector((state) => state.cart.games);
+	const cartTotal = useAppSelector((state) => state.cart.cartTotal);
+	const minCartValue = useAppSelector((state) => state.cart.minCartValue);
+	const games = useAppSelector((state) => state.games.list);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+
+	const { newBet } = betsServices();
+
+	const [cartModal, setCartModal] = useState(<Modal />);
+	const [showModal, setShowModal] = useState(false);
+
+	const closeModal = () => {
+		setShowModal(false);
+	};
+
+	const confirmBuy = async () => {
+		console.log(gamesInCart);
+		setShowModal(false);
+		try {
+			const response = await toast.promise(newBet({ games: gamesInCart }), {
+				pending: 'Carregando',
+				success: 'Compra realizada com sucesso',
+				error: 'Falha na compra',
+			});
+
+			dispatch(asyncAddBets('/bet/all-bets?'));
+
+			navigate('/home', { replace: true });
+		} catch (error: any) {
+			if (error.status === 401) {
+				toast.error(error.data.message);
+			}
+		}
+	};
+
+	const goToCartHandler = () => {
+		if (cartTotal >= minCartValue) {
+			console.log('confirmar', cartTotal, cartModal, showModal);
+			setShowModal(true);
+			setCartModal(
+				<Modal onClose={closeModal}>
+					<ContentModal>
+						<IoAlertCircleOutline />
+						<p>
+							Your cart has a total{' '}
+							<strong>{valueFormat('PRICE', cartTotal.toString())} </strong>
+							of in games. Do you want to complete the buy?
+						</p>
+						<div>
+							<ButtonModal onClick={confirmBuy}>Confirm</ButtonModal>
+							<ButtonModal onClick={closeModal}>Cancel</ButtonModal>
+						</div>
+					</ContentModal>
+				</Modal>
+			);
+		} else {
+			console.log('ainda falta', cartTotal, minCartValue - cartTotal);
+			setShowModal(true);
+			setCartModal(
+				<Modal onClose={closeModal}>
+					<ContentModal>
+						<IoAlertCircleOutline />
+						<p>
+							Minimum value of{' '}
+							<strong>{valueFormat('PRICE', minCartValue.toString())} </strong>
+							not reached, please add more{' '}
+							<strong>
+								{valueFormat('PRICE', (minCartValue - cartTotal).toString())}
+							</strong>{' '}
+							in games to cart to complete purchase
+						</p>
+						<ButtonModal onClick={closeModal}>OK</ButtonModal>
+					</ContentModal>
+				</Modal>
+			);
+		}
+	};
 
 	return (
 		<ContainerCart>
 			<h1>Cart</h1>
-
 			{gamesInCart.length === 0 && (
 				<EmptyMessage
 					message='No games added to cart'
@@ -19,11 +119,50 @@ const Cart = () => {
 				/>
 			)}
 
-			// Games in cart
+			<GamesContainer>
+				{gamesInCart.map((game, index) => {
+					const price = getGamePriceNumber(games, game.game_id);
+
+					const removeToCartHandler = () => {
+						dispatch(removeToCart({ index, price }));
+					};
+
+					return (
+						<Content>
+							<ButtonTrash onClick={removeToCartHandler}>
+								<IoTrashOutline />
+							</ButtonTrash>
+							<GameInfo color={getGameColor(games, game.game_id)}>
+								<p className='gameNumbers'>
+									{valueFormat('NUMBERS', game.numbers.join(','))}
+								</p>
+								<div>
+									<p className='gameName'>{getGameName(games, game.game_id)}</p>
+									<p>{getGamePrice(games, game.game_id)}</p>
+								</div>
+							</GameInfo>
+						</Content>
+					);
+				})}
+			</GamesContainer>
+
+			<CartTotal>
+				<p>
+					<strong>CART </strong> TOTAL:{' '}
+					{valueFormat('PRICE', cartTotal.toString())}
+				</p>
+			</CartTotal>
 
 			<ButtonSave>
-				<Button goTo={'Save'} fontSize={35} color={'var(--light-green)'} />
+				<Button
+					goTo={'Save'}
+					fontSize={35}
+					color={'var(--light-green)'}
+					onGoTo={goToCartHandler}
+				/>
 			</ButtonSave>
+
+			{showModal && cartModal}
 		</ContainerCart>
 	);
 };
--- src/components/Cart/style.ts
+++ src/components/Cart/style.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface IContentProps {
+	color: string;
+}
+
 export const ContainerCart = styled.div`
 	width: 317px;
 	height: 484px;
@@ -7,9 +11,9 @@ export const ContainerCart = styled.div`
 	background-color: var(--white);
 	margin-top: 4rem;
 	color: var(--grey);
-   display: flex;
-   flex-direction: column;
-   justify-content: space-between;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 
 	h1 {
 		font-style: italic;
@@ -21,10 +25,128 @@ export const ContainerCart = styled.div`
 
 export const ButtonSave = styled.div`
 	display: flex;
-   justify-content: center;
-   align-items: center;
+	justify-content: center;
+	align-items: center;
 	background-color: var(--grey-button-save);
-   border: 2px solid var(--grey-button-save-border);
-   border-bottom-left-radius: inherit;
-   border-bottom-right-radius: inherit;
+	border: 2px solid var(--grey-button-save-border);
+	border-bottom-left-radius: inherit;
+	border-bottom-right-radius: inherit;
+`;
+
+export const CartTotal = styled.div`
+	color: var(--grey);
+	font-size: 24px;
+	text-align: center;
+
+	strong {
+		font-style: italic;
+	}
+`;
+
+export const GamesContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	overflow: auto;
+
+	::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	::-webkit-scrollbar-track {
+		background: var(--light-grey);
+	}
+
+	::-webkit-scrollbar-thumb {
+		background-color: var(--grey);
+		border-radius: 10px;
+		border: 2px solid var(--light-grey);
+	}
+`;
+
+export const Content = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 4fr;
+	margin-bottom: 1rem;
+
+	p {
+		margin: 0.5rem;
+	}
+`;
+
+export const GameInfo = styled.div<IContentProps>`
+	border-left: 5px solid ${(props) => props.color};
+	border-radius: 5px;
+
+	div {
+		display: flex;
+		flex-direction: row;
+		align-items: flex-end;
+		gap: 0;
+		padding-left: 0.3rem;
+	}
+
+	.gameName,
+	.gameNumbers {
+		font-style: italic;
+		font-weight: bold;
+	}
+
+	.gameName {
+		color: ${(props) => props.color};
+		font-size: 16px;
+	}
+
+	.gameNumbers {
+		padding-left: 0.5rem;
+	}
+`;
+
+export const ButtonTrash = styled.button`
+	background-color: transparent;
+	border: none;
+	font-size: 24px;
+	color: var(--grey);
+`;
+
+export const ContentModal = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	color: var(--grey);
+	font-weight: bold;
+	font-style: italic;
+	text-align: center;
+
+	svg {
+		font-size: 50px;
+		color: var(--green);
+	}
+
+	p strong {
+		color: var(--green);
+	}
+
+	div {
+		display: inherit;
+		flex-direction: row;
+		gap: 1rem;
+	}
+`;
+
+export const ButtonModal = styled.button`
+	width: 113px;
+	height: 34px;
+	border-radius: 100px;
+	border: 2px solid var(--green);
+	font-style: italic;
+	font-weight: bold;
+	text-transform: capitalize;
+	color: var(--green);
+	background-color: transparent;
+
+	:hover {
+		color: var(--white);
+		background-color: var(--green);
+	}
 `;
